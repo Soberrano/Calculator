c291330ec72cfa1048ae2c867caca1751f2fd9bf
Игорь Кривенко <igor125k@mail.ru>
2018-04-10 15:22:20 +0300

    обратная польская запись

--- Calculator/MainWindowViewModel.cs
+++ Calculator/MainWindowViewModel.cs
@@ -13,82 +13,102 @@ namespace Calculator
     class MainWindowViewModel : INotifyPropertyChanged
     {
         Model _model = new Model();
-        bool resultClicked = false;
+        string textShow;
         bool operationClicked = false;
         public MainWindowViewModel()
         {
-            #region Реализация команд
+
+            //#region Реализация команд
             ShowText = new Command((param) =>
             {
-                if (!resultClicked)
+                textShow += CommandParameter;
+                Text = textShow;
+                if (!operationClicked)
                 {
-                    Text += CommandParameter;
-                    if (!operationClicked)
+                    try
                     {
-                        _model.Result = double.Parse(Text);
                         _model.ResultM = double.Parse(Text);
                     }
-                    if (operationClicked)
-                        _model.SecondNumber = double.Parse(Text);
+                    catch { }
                 }
-                else//если нажато "=" => выведи результат
-                    Text = Convert.ToString(_model.GetResult());
+                if (operationClicked)
+                    try
+                    {
+                        _model.SecondNumber = double.Parse(Text);
+                    }
+                    catch { }
 
-                operationClicked = false;
             }, (param) => true);
-            //Реализация команды для операторов
+            ////Реализация команды для операторов
             GetOperation = new Command((param) =>
             {
-                _model.Operation = CommandParameter;
-                operationClicked = true;
-                resultClicked = false;
-                _model.Result = _model.GetResult();
+                textShow += CommandParameter;
+                TextMem += textShow;
+                textShow = "";
                 Text = "";
+
             }, (param) => true);
-            //Реализация команды для операторов "M..."
-            GetOperation = new Command((param) =>
+            ////Реализация команды для операторов "M..."
+            GetOperationM = new Command((param) =>
             {
                 _model.Operation = CommandParameter;
                 operationClicked = true;
-                resultClicked = false;
                 _model.ResultM = _model.GetResultM();
+                textShow = "";
                 Text = "";
             }, (param) => true);
-            //реализация команды для "="
+            ////реализация команды для "="
             ShowResult = new Command((param) =>
             {
-                _model.OnlyFirstnumber = false;
-                resultClicked = true;
-                _model.Result = _model.GetResult();
+                TextMem += textShow;
+                _model.Result = _model.Calculate(TextMem);
                 Text = Convert.ToString(_model.Result);
             }, (param) => true);
+
             //реализация команды для "MR"
             ShowResultM = new Command((param) =>
             {
                 _model.OnlyFirstnumber = false;
-                resultClicked = true;
                 _model.ResultM = _model.GetResultM();
-                Text = Convert.ToString(_model.ResultM);
-                _model.Result = _model.ResultM;
-                _model.First = false;
+                textShow = Convert.ToString(_model.ResultM);
+                Text = textShow;
             }, (param) => true);
-
-            //Реализация команды для сброса
+            //Реализация команды для сброса C
             Clear = new Command((param) =>
             {
                 _model.Result = 0;
                 _model.SecondNumber = 0;
-                resultClicked = false;
+                TextMem = "";
                 Text = "";
+                textShow = "";
                 operationClicked = false;
+            }, (param) => true);
+            //Реализация команды для сброса CЕ
+
+            ClearMem = new Command((param) =>
+            {
+                _model.Result = 0;
+                _model.SecondNumber = 0;
+                Text = "";
+                textShow = "";
+                operationClicked = false;
+            }, (param) => true);
+            //Реализация команды для сброса Del
+            ClearDel = new Command((param) =>
+            {
+                try
+                {
+                    int i = Text.Length - 1;
+                    Text = Text.Remove(i);
+                    textShow = "";
+                }
+                catch { };
             }, (param) => true);
             //Реализация команды для MC
             ClearM = new Command((param) =>
             {
                 _model.ResultM = 0;
                 _model.SecondNumber = 0;
-                resultClicked = false;
-                Text = "";
                 operationClicked = false;
             }, (param) => true);
 
@@ -98,7 +118,7 @@ namespace Calculator
                 Text += CommandParameter;
                 if (!operationClicked)
                 {
-                    
+
                 }
 
             }, (param) =>
@@ -107,7 +127,7 @@ namespace Calculator
                     return true;
                 else return false;
             });
-            #endregion
+
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -121,6 +141,20 @@ namespace Calculator
             set
             {
                 text = value;
+                OnPropertyChanged();
+                Point.InvokeCanExecChanged();
+            }
+        }
+        string textMem;
+        public string TextMem
+        {
+            get
+            {
+                return textMem;
+            }
+            set
+            {
+                textMem = value;
                 OnPropertyChanged();
                 Point.InvokeCanExecChanged();
             }
@@ -174,6 +208,10 @@ namespace Calculator
         public Command ShowResultM
         { get; set; }
         public Command Clear
+        { get; set; }
+        public Command ClearMem
+        { get; set; }
+        public Command ClearDel
         { get; set; }
         public Command ClearM
         { get; set; }
--- Calculator/Model.cs
+++ Calculator/Model.cs
@@ -17,109 +17,299 @@ namespace Calculator
         public double Num { get; set; }
         public bool First { get; set; } = true;
 
-        public double GetResult()
+        //public double GetResult()
+        //{
+
+        //    switch (Operation)
+        //    {
+        //        case "+":
+        //            Result = Result + SecondNumber;
+
+        //            break;
+        //        case "-":
+        //            Result = Result - SecondNumber;
+
+        //            break;
+        //        case "/":
+        //            if (First == true)
+        //            {
+        //                First = false;
+        //            }
+        //            else
+        //            {
+        //                Result = Result / SecondNumber;
+        //                First = true;
+        //            }
+
+        //            break;
+        //        case "*":
+        //            if (First == true)
+        //            {
+        //                First = false;
+        //            }
+        //            else
+        //            {
+        //                Result = Result * SecondNumber;
+        //                First = true;
+        //            }
+        //            break;
+        //        case "^":
+        //            if (First == true)
+        //            {
+        //                First = false;
+        //            }
+        //            else
+        //            {
+        //                Result = Math.Pow((Result), SecondNumber);
+        //                OnlyFirstnumber = true;
+        //                First = true;
+        //            }
+        //            break;
+        //        case "sin":
+        //            Result = Math.Sin(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "cos":
+        //            Result = Math.Cos(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "tan":
+        //            Result = Math.Tan(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "ctg":
+        //            Result = 1 / (Math.Tan(Result));
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "ln":
+        //            Result = Math.Log(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "log":
+        //            Result = Math.Log10(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+        //        case "x!":
+        //            double a = 1;
+        //            for (int i = 1; i <= Result; i++)
+        //            {
+        //                a = a * i;
+        //            }
+        //            Result = a;
+        //            OnlyFirstnumber = true;
+        //            Operation = "факториал от ";
+        //            break;
+        //        case "x^2":
+        //            Result = Math.Pow((Result), 2);
+        //            OnlyFirstnumber = true;
+        //            Operation = "квадрат от ";
+        //            break;
+        //        case "√":
+        //            Result = Math.Sqrt(Result);
+        //            OnlyFirstnumber = true;
+        //            break;
+
+        //        case "1/x":
+        //            Result = 1 / (Result);
+        //            OnlyFirstnumber = true;
+        //            Operation = "1/";
+        //            break;
+        //    }
+        //    return Result;
+        //}
+
+
+        #region new era
+        //Метод возвращает true, если проверяемый символ - разделитель ("пробел" или "равно")
+
+        static private bool IsDelimeter(char c)
         {
-            switch (Operation)
-            {
-                case "+":
-                    Result = Result + SecondNumber;
-
-                    break;
-                case "-":
-                    Result = Result - SecondNumber;
-
-                    break;
-                case "/":
-                    if (First == true)
-                    {
-                        First = false;
-                    }
-                    else
-                    {
-                        Result = Result / SecondNumber;
-                        First = true;
-                    }
-
-                    break;
-                case "*":
-                    if (First == true)
-                    {
-                        First = false;
-                    }
-                    else
-                    {
-                        Result = Result * SecondNumber;
-                        First = true;
-                    }
-                    break;
-                case "^":
-                    if (First == true)
-                    {
-                        First = false;
-                    }
-                    else
-                    {
-                        Result = Math.Pow((Result), SecondNumber);
-                        OnlyFirstnumber = true;
-                        First = true;
-                    }
-                    break;
-                case "sin":
-                    Result = Math.Sin(Result);
-                    OnlyFirstnumber = true;
-                    break;
-                case "cos":
-                    Result = Math.Cos(Result);
-                    OnlyFirstnumber = true;
-                    break;
-                case "tan":
-                    Result = Math.Tan(Result);
-                    OnlyFirstnumber = true;
-                    break;
-                case "ctg":
-                    Result = 1 / (Math.Tan(Result));
-                    OnlyFirstnumber = true;
-                    break;
-                case "ln":
-                    Result = Math.Log(Result);
-                    OnlyFirstnumber = true;
-                    break;
-                case "log":
-                    Result = Math.Log10(Result);
-                    OnlyFirstnumber = true;
-                    break;
-                case "x!":
-                    double a = 1;
-                    for (int i = 1; i <= Result; i++)
-                    {
-                        a = a * i;
-                    }
-                    Result = a;
-                    OnlyFirstnumber = true;
-                    Operation = "факториал от ";
-                    break;
-                case "x^2":
-                    Result = Math.Pow((Result), 2);
-                    OnlyFirstnumber = true;
-                    Operation = "квадрат от ";
-                    break;
-                case "√":
-                    Result = Math.Sqrt(Result);
-                    OnlyFirstnumber = true;
-                    break;
-
-                case "1/x":
-                    Result = 1 / (Result);
-                    OnlyFirstnumber = true;
-                    Operation = "1/";
-                    break;
-            }
-            return Result;
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
         }
+        //Метод возвращает true, если проверяемый символ - оператор
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*^()!sctklg√".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        //Метод возвращает приоритет оператора
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '^': return 5;
+                case '!': return 5;
+                case 's': return 6;
+                case 'c': return 6;
+                case 't': return 6;
+                case 'k': return 6;//ctg
+                case 'l': return 6;
+                case 'g': return 6;//log
+                case '√': return 7;
+                default: return 8;
+            }
+        }
+
+        //"Входной" метод класса
+        public double Calculate(string input)
+        {
+            string output = GetExpression(input); //Преобразовываем выражение в постфиксную запись
+            double result = Counting(output); //Решаем полученное выражение
+            return result; //Возвращаем результат
+        }
+
+        static private string GetExpression(string input)
+        {
+            string output = string.Empty; //Строка для хранения выражения
+            Stack<char> operStack = new Stack<char>(); //Стек для хранения операторов
+
+            for (int i = 0; i < input.Length; i++) //Для каждого символа в входной строке
+            {
+                //Разделители пропускаем
+                if (IsDelimeter(input[i]))
+                    continue; //Переходим к следующему символу
+
+                //Если символ - цифра, то считываем все число
+                if (Char.IsDigit(input[i])) //Если цифра
+                {
+                    //Читаем до разделителя или оператора, что бы получить число
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i]; //Добавляем каждую цифру числа к нашей строке
+                        i++; //Переходим к следующему символу
+
+                        if (i == input.Length) break; //Если символ - последний, то выходим из цикла
+                    }
+
+                    output += " "; //Дописываем после числа пробел в строку с выражением
+                    i--; //Возвращаемся на один символ назад, к символу перед разделителем
+                }
+
+                //Если символ - оператор
+                if (IsOperator(input[i])) //Если оператор
+                {
+                    if (input[i] == '(') //Если символ - открывающая скобка
+                        operStack.Push(input[i]); //Записываем её в стек
+                    else if (input[i] == ')') //Если символ - закрывающая скобка
+                    {
+                        //Выписываем все операторы до открывающей скобки в строку
+                        char s = operStack.Pop();
+
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else //Если любой другой оператор
+                    {
+                        if (operStack.Count > 0) //Если в стеке есть элементы
+                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek())) //И если приоритет нашего оператора меньше или равен приоритету оператора на вершине стека
+                                output += operStack.Pop().ToString() + " "; //То добавляем последний оператор из стека в строку с выражением
+
+                        operStack.Push(char.Parse(input[i].ToString())); //Если стек пуст, или же приоритет оператора выше - добавляем операторов на вершину стека
+
+                    }
+                }
+            }
+
+            //Когда прошли по всем символам, выкидываем из стека все оставшиеся там операторы в строку
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output; //Возвращаем выражение в постфиксной записи
+        }
+
+        static private double Counting(string input)
+        {
+            double result = 0; //Результат
+            Stack<double> temp = new Stack<double>(); //Dhtvtyysq стек для решения
+
+            for (int i = 0; i < input.Length; i++) //Для каждого символа в строке
+            {
+                //Если символ - цифра, то читаем все число и записываем на вершину стека
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) //Пока не разделитель
+                    {
+                        a += input[i]; //Добавляем
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a)); //Записываем в стек
+                    i--;
+                }
+                else if (IsOperator(input[i])) //Если символ - оператор
+                {
+                    //Берем два последних значения из стека
+                    double a = temp.Pop();
+                    double b;
+                    try
+                    {
+                        if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '^')
+                        {
+                            b = temp.Pop();
+                            switch (input[i]) //И производим над ними действие, согласно оператору
+                            {
+                                case '+': result = b + a; break;
+                                case '-': result = b - a; break;
+                                case '*': result = b * a; break;
+                                case '/': result = b / a; break;
+                                case '^': result = double.Parse(Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()); break;
+                            }
+                        }
+                        switch (input[i])
+                        {
+                            case '!':
+                                {
+                                    double o = 1;
+                                    for (int h = 1; h <= a; h++)
+                                    {
+                                        o = o * h;
+                                    }
+                                    return o;
+                                }
+                            case 's': result = (Math.Sin(a)); break;
+                            case 'c': result = (Math.Cos(a)); break;
+                            case 't': result = (Math.Tan(a)); break;
+                            case 'k': result = 1 / (Math.Tan(a)); break;//ctg
+                            case 'l': result = (Math.Log(a)); break;
+                            case 'g': result = (Math.Log10(a)); break;//log
+                            case '√': result = Math.Sqrt(a); break;
+                        }
+
+                    }
+                    catch { };
+
+                    temp.Push(result); //Результат вычисления записываем обратно в стек
+                }
+            }
+            return temp.Peek(); //Забираем результат всех вычислений из стека и возвращаем его
+        }
+        #endregion
+
+
+
+
+
+
+
 
         public double GetResultM()
         {
-            switch(Operation)
+            switch (Operation)
             {
                 case "M+":
                     ResultM = ResultM + SecondNumber;
@@ -133,3 +323,4 @@ namespace Calculator
         }
     }
 }
+
